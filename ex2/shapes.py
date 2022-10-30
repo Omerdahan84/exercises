b4eeb175f5722020ed5a7ac790890bc82bfa91de
omer.dahan <omer111dahan@gmail.com>
2022-10-30 21:00:52 +0200

    Added a finctions to implement shape_area in shape.py

--- ex2/shapes.py
+++ ex2/shapes.py
@@ -16,16 +16,16 @@ def shape_area():
         #it's necessary to cast the input because it's type is str
         #which not allow numeric calculations
         radius = float(input())
-        #returns the area of a circle according to circle area formula
-        return math.pi * radius**2
+        #call the function circle are to calculate the area
+        return circle_area(radius)
     #Checks if the inputs is equal to '2'
     elif user_input == '2':
         #storing the values of the sides of the trctangle 
         #necessary to cast - same reason as before
         first_side = float(input())
         second_side = float(input())
-        #returns and calculates the area according to the formula
-        return first_side * second_side
+        #call the function rectangle_area to calculate the area
+        return rectangle_area(first_side, second_side)
     #check if user input is equal to 3
     elif user_input == '3':
         #storing the value of the side of the triangle
@@ -33,4 +33,20 @@ def shape_area():
         side = float(input())
         #return the value of area according to the formula of triangle
         #with equal sides
-        return (3**0.5 / 4) * side**2
+        #call the function triangle_area to calculate the area
+        return triangle_area(side)
+        
+def circle_area(radius):
+    """This function gets a radius of a circle and return its area 
+    according to the formula"""
+    #returns the area of a circle according to circle area formula
+    return math.pi * radius**2
+def rectangle_area(first_side, second_side):
+    """This function gets two sides of rectangle and returns
+    it's area"""
+    #returns and calculates the area according to the formula
+    return first_side * second_side
+def triangle_area(side):
+    """This function gets the side of a triangle with equal sides
+    and returns his area according to the formula"""
+    return (3 ** 0.5 / 4) * side**2
