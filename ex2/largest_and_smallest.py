f49adb98e2574eb335634a1bb4623f9985723be6
omer.dahan <omer111dahan@gmail.com>
2022-10-30 17:24:58 +0200

    fixing largest_and_smallest.py name

--- ex2/largest_and_smallest.py
+++ ex2/largest_and_smallest.py
@@ -1,22 +1,22 @@
 def largest_and_smallest(n1, n2, n3):
     """This function gets three numbers and returns the smallest
     and biggest value among them"""
+    #Sets the intial value of the maximun to n1
     max_val = n1
-
+    #if n2 is bigger we set the maximim to be n2
     if n2 > max_val:
         max_val = n2
+    #if n3 is bigger we set the maximum to be n3
     if n3 > max_val:
         max_val = n3
-
+    #Sets the intial value of the minimum to n1
     min_val = n1
-
+    #if n2 is smaller from the min_value we set min_val to be
+    #equal to =n2
     if n2 < min_val:
         min_val = n2
+    #if n3 is smaller from the initial value we set min_val to be
+    #equal to n3
     if n3 < min_val:
         min_val = n3
     return max_val,min_val
-
-if __name__ == '__main__':
-    max_val, min_val = largest_and_smallest(155, 11, 11)
-    print(max_val) # result is 10
-    print(min_val) # result is 1
