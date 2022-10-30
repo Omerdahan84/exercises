8cff4fe3e06cac79c5a6d344dd3ce4fcf47a465d
omer.dahan <omer111dahan@gmail.com>
2022-10-30 19:01:07 +0200

    Added the file shape.py which includes the function shape_area

---
+++ ex2/shapes.py
@@ -0,0 +1,17 @@
+import math
+
+def shape_area():
+    user_input = input("Choose shape (1=circle, 2=rectangle, 3=triangle): ")
+    if user_input == '1':
+        radius = float(input(("Insert the radius of the circle: ")))
+        return math.pi * radius**2
+    elif user_input == '2':
+        first_side = float(input(("Insert the first side of the rectangle: ")))
+        second_side = float(input(("Insert the second side of the rectangle: ")))
+        return first_side * second_side
+    elif user_input == '3':
+        side = float(input(("Insert the side of the triangle: ")))
+        return (3**0.5 / 4) * side**2
+if __name__ == '__main__':
+    print(shape_area())
+
