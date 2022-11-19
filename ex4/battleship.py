4b8d21902595eac65fc6a43d364c2f6449ece79d
omer.dahan <omer111dahan@gmail.com>
2022-11-19 14:32:02 +0200

    last

--- ex4/battleship.py
+++ ex4/battleship.py
@@ -1,7 +1,7 @@
 import helper
 
 
-def init_board(rows = helper.NUM_ROWS, columns = helper.NUM_COLUMNS):
+def init_board(rows=helper.NUM_ROWS, columns=helper.NUM_COLUMNS):
     """This function intialzaing an empty board"""
     # Using list comprehension to create a list with the sign for empty cell
     # this list will pass as a paramer to a list comprehension to create
@@ -9,37 +9,40 @@ def init_board(rows = helper.NUM_ROWS, columns = helper.NUM_COLUMNS):
     board = [[helper.WATER for i in range(columns)] for j in range(rows)]
     return board
 
+
 def check_row_col(row, col):
     """ This funcion checks if the rows or columns of specific
     cell are in the board"""
-    if row >= helper.NUM_ROWS or row < 0  or col >=  helper.NUM_COLUMNS or \
-        col < 0:
+    if row >= helper.NUM_ROWS or row < 0 or col >= helper.NUM_COLUMNS or \
+            col < 0:
         return False
     else:
         return True
+
 
 def cell_loc(name):
     """This function gets a string represent a location in the board and
     return the tuple that fits to that location in the board matrix"""
     # Checks if the first value of the string is alphabetic
-    #if name not in range(2,4):
-        #return None
+    # if name not in range(2,4):
+    # return None
     if len(name) <= 1 or len(name) > 3:
         return None
     if name[0].isalpha() == False:
         return None
     else:
         # If yes checks the last of the input can be cast to int
-        for i in range(1,len(name)):
+        for i in range(1, len(name)):
             if helper.is_int(name[i]) == False:
                 return None
     column = name[0].upper()
     row = name[1:]
     row = int(name[1:]) - 1
     column = (ord(column) % 65)
-    if check_row_col(row,column) == False:
+    if check_row_col(row, column) == False:
         return None
     return row, column
+
 
 def valid_ship(board, size, loc):
     """This function gets a board size of a ship and a location indicates
@@ -53,14 +56,15 @@ def valid_ship(board, size, loc):
             return False
     return True
 
-def create_player_board(rows = helper.NUM_ROWS, \
-    columns = helper.NUM_COLUMNS, ship_sizes = helper.SHIP_SIZES):
+
+def create_player_board(rows=helper.NUM_ROWS,
+                        columns=helper.NUM_COLUMNS, ship_sizes=helper.SHIP_SIZES):
     """This function sets submrines in specifics sides in location the user 
     choose, if location is not valid the function will ask for another one"""
-    board = init_board(rows, columns) # Initializing an empty board 
-    # going over the tuple of ships sizes 
+    board = init_board(rows, columns)  # Initializing an empty board
+    # going over the tuple of ships sizes
     for size in ship_sizes:
-        # Asking from the user from a valid location until the location 
+        # Asking from the user from a valid location until the location
         # provided is valid
         while True:
             helper.print_board(board)
@@ -70,7 +74,7 @@ ship of size {size}: ')
             if loc == None:
                 print("Invalid location")
                 continue
-            if valid_ship(board,size, loc) == False:
+            if valid_ship(board, size, loc) == False:
                 print("Invalid location")
                 continue
             for i in range(size):
@@ -78,20 +82,22 @@ ship of size {size}: ')
             break
     return board
 
+
 def check_avalibale_spots(board, size):
     """This function gets the  board and return a list of tuples, 
     each tuple represent avalibalse spot"""
-    
+
     st = set()
     for i in range(len(board)):
         for j in range(len(board[i])):
-            if board[i][j] == helper.WATER and i + size <= len(board) :
+            if board[i][j] == helper.WATER and i + size <= len(board):
                 loc = (i, j)
                 st.add(loc)
     return st
-    
-def create_computer_board( rows = helper.NUM_ROWS, \
-    columns = helper.NUM_COLUMNS, ship_sizes = helper.SHIP_SIZES):
+
+
+def create_computer_board(rows=helper.NUM_ROWS,
+                          columns=helper.NUM_COLUMNS, ship_sizes=helper.SHIP_SIZES):
     """Gets the player board and chhose a place to put 
     submarines for the computer"""
     board = init_board(rows, columns)
@@ -101,17 +107,18 @@ def create_computer_board( rows = helper.NUM_ROWS, \
         # ship size
         free_spots = check_avalibale_spots(board, size)
         while True:
-            
+
             cell = helper.choose_ship_location(board, size, free_spots)
             row = cell[0]
             col = cell[1]
-            if valid_ship(board,size,cell) != True:
+            if valid_ship(board, size, cell) != True:
                 continue
             else:
                 for i in range(size):
                     board[row + i][col] = helper.SHIP
-                break      
+                break
     return board
+
 
 def fire_torpedo(board, loc):
     """This function gets a board and a location to fire torpado
@@ -119,15 +126,16 @@ def fire_torpedo(board, loc):
     row = loc[0]
     col = loc[1]
     if check_row_col(row, col) == False or  \
-        board[row][col] == helper.HIT_WATER\
-        or  board[row][col] == helper.HIT_SHIP:
+            board[row][col] == helper.HIT_WATER\
+            or board[row][col] == helper.HIT_SHIP:
         return board
     if board[row][col] == helper.SHIP:
         board[row][col] = helper.HIT_SHIP
     else:
         board[row][col] = helper.HIT_WATER
     return board
-    
+
+
 def game_finished(board):
     """Checks if thera are ships in the board
     to detemine if the game finished"""
@@ -137,6 +145,7 @@ def game_finished(board):
             if item == helper.SHIP:
                 return False
     return True
+
 
 def turpedo_spots(board):
     """This function gets the player board and return a list of tuples, 
@@ -149,10 +158,11 @@ def turpedo_spots(board):
                 st.add(loc)
     return st
 
+
 def set_hidden_board(computer_board):
     """Gets the computer board and returns an hidden board. represen ships as 
     water."""
-    hidden_board = init_board(len (computer_board), len(computer_board[0]))
+    hidden_board = init_board(len(computer_board), len(computer_board[0]))
     for i in range(len(computer_board)):
         for j in range(len(computer_board[i])):
             if computer_board[i][j] != helper.SHIP:
@@ -161,12 +171,14 @@ def set_hidden_board(computer_board):
                 hidden_board[i][j] = helper.WATER
     return hidden_board
 
+
 def main():
     """Runs the game"""
-    player_board = create_player_board() #Creating the user board
-    computer_board = create_computer_board() #Creating the computer board
-    hidden_board = set_hidden_board(computer_board)# Hiding the computer ships
-    run_game = True #Boolean value to running the game
+    player_board = create_player_board()  # Creating the user board
+    computer_board = create_computer_board()  # Creating the computer board
+    hidden_board = set_hidden_board(
+        computer_board)  # Hiding the computer ships
+    run_game = True  # Boolean value to running the game
     # Loops until game over
     while run_game:
         hidden_board = set_hidden_board(computer_board)
@@ -179,7 +191,7 @@ def main():
             else:
                 loc = cell_loc(hit)
                 if computer_board[loc[0]][loc[1]] == helper.HIT_WATER\
-                    or computer_board[loc[0]][loc[1]] == helper.HIT_SHIP:
+                        or computer_board[loc[0]][loc[1]] == helper.HIT_SHIP:
                     print("You already choosed this location.")
                     continue
                 else:
@@ -199,13 +211,13 @@ def main():
             while True:
                 another_round = helper.get_input("The computer won, would you \
 like to play another round ('Y' = yes, 'N' = no): ")
-                if another_round != 'Y' and  another_round != 'N':
+                if another_round != 'Y' and another_round != 'N':
                     print("invalid answer")
                 else:
                     run_game = False
                     break
         # if the computer loses
-        elif game_finished(computer_board): 
+        elif game_finished(computer_board):
             helper.print_board(player_board, computer_board)
             # take input to check if the user wants to play another round
             while True:
@@ -219,6 +231,6 @@ like to play another round ('Y' = yes, 'N' = no): ")
     if another_round == 'Y':
         main()
 
+
 if __name__ == "__main__":
     main()
-    
---
+++ ex4/p.py
@@ -0,0 +1,4 @@
+counter: float = 300
+counter = counter / 2
+for i in range(counter):
+    print(i)
